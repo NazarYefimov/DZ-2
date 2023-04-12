68222b21e11d5352fd91f400fac559ec73c0b035
NazarYefimov <123975599+NazarYefimov@users.noreply.github.com>
2023-04-12 17:33:28 +0300

    MyQueue

---
+++ MyQueue2.0.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyQueue<T> {
+    private List<T> queue = new ArrayList<>();
+
+    public MyQueue() {
+    }
+
+    public void add(T value) {
+        queue.add(value);
+    }
+
+    public void clear() {
+        queue.clear();
+    }
+
+    public int size() {
+        return queue.size();
+    }
+
+    public T peek() {
+        if (!queue.isEmpty()) {
+            return queue.get(0);
+        }
+        return null;
+    }
+
+    public T poll() {
+        if (!queue.isEmpty()) {
+            return queue.remove(0);
+        }
+        return null;
+    }
+}
